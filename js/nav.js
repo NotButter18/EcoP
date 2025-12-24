6d13cdf333fbf8cc9af19fe0c8043a2494d3bba2
Nimaan Buttar <168145054+NotButter18@users.noreply.github.com>
2025-12-23 21:58:01 -0500

    Add files via upload

--- js/nav.js
+++ js/nav.js
@@ -17,3 +17,24 @@ document.querySelectorAll('a').forEach(link => {
         }
     });
 });
+
+const modal = document.getElementById("data-modal");
+const btn = document.getElementById("open-modal");
+const span = document.getElementsByClassName("close-modal")[0];
+
+// Open the pop-up
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Close the pop-up
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Close if they click outside the box
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
